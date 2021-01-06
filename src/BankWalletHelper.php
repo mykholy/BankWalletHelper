cf6787b9477b67ac0ab8a10e97f3c52b1cf37100
mykholy <mykholys30@gmail.com>
2021-01-06 16:21:59 +0200

    =add curl api

--- src/BankWalletHelper.php
+++ src/BankWalletHelper.php
@@ -5,11 +5,70 @@ namespace Mykholy\BankWalletHelper;
 class BankWalletHelper
 {
 
-    public function greet($greet = "Hello World")
+    private $URL = "http://localhost/bank/";
+    private $client_id;
+    private $client_secret;
+
+    public function __construct($client_id, $client_secret)
     {
-        return $greet;
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
     }
 
+    public function checkout($data)
+    {
 
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->URL . 'api/v1/merchants/payment/checkout',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPHEADER => array(
+                'client-id: ' . $this->client_id,
+                'client-secret: ' . $this->client_secret,
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+
+    }
+
+    public function doCheckoutPayment($transaction_id)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->URL . 'api/v1/merchants/payment/doCheckoutPayment',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('transaction_id' => $transaction_id),
+            CURLOPT_HTTPHEADER => array(
+                'client-id: ' . $this->client_id,
+                'client-secret: ' . $this->client_secret
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
 
 }
